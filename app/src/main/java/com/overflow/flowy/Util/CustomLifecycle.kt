032a81994955104d821f30e94fd6e14bd4028128
shindonghwi <ehdgnl8940@naver.com>
2020-10-26 09:51:17 +0900

    커스텀 라이프사이클 클래스 설명 추가

--- app/src/main/java/com/overflow/flowy/Util/CustomLifecycle.kt
+++ app/src/main/java/com/overflow/flowy/Util/CustomLifecycle.kt
@@ -4,6 +4,9 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
 
+
+/** 카메라 x의 수명주기에 바인딩 할때 사용하는, 라이프 사이클 관리 클래스이다. */
+
 class CustomLifecycle : LifecycleOwner {
     private val mLifecycleRegistry: LifecycleRegistry = LifecycleRegistry(this)
     fun doOnResume() {
